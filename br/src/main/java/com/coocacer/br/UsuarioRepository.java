22a00a594d4149da5722472d76aa5892a8049395
tecnologia-cmd <tecnologia@coocaceraraguari.com.br>
2026-02-05 17:45:10 -0300

    Corrigindo imports do usuario admin

--- br/src/main/java/com/coocacer/br/UsuarioRepository.java
+++ br/src/main/java/com/coocacer/br/UsuarioRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     
     // Busca usuário pelo login (usado no login)
-    Usuario findByLogin(String login);
+Usuario findByLogin(String login);
 
     // ✅ NOVO: Busca usuário pelo nome (usado para validar CNH na reserva)
     Usuario findByNome(String nome);
